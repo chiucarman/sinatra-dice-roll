142645510521cc31f306a473ff0f50cebf5b3423
Carman Chiu <101212528+chiucarman@users.noreply.github.com>
2023-08-09 04:32:01 +0000

    added /dice/5/4 link and support request

--- dice.rb
+++ dice.rb
@@ -16,6 +16,7 @@ get("/") do
     <li><a href=\"/dice/2/6\">Roll two 6-sided dice</a></li>
     <li><a href=\"/dice/2/10\">Roll two 10-sided dice</a></li>
     <li><a href=\"/dice/1/20\">Roll one 20-sided dice</a></li>
+    <li><a href=\"/dice/5/4\">Roll five 4-sided dice</a></li>
   </ul>
   "
 end
@@ -57,5 +58,19 @@ get("/dice/1/20") do
   outcome = "You rolled a #{first_die} for a total of #{sum}."
 
   "<h1>1d20</h1>
+  <p>#{outcome}</p>"
+end
+
+get("/dice/5/4") do
+  first_die = rand(1..4)
+  second_die = rand(1..4)
+  third_die = rand(1..4)
+  fourth_die = rand(1..4)
+  fifth_die = rand(1..4)
+  sum = first_die + second_die + third_die + fourth_die + fifth_die
+
+  outcome = "You rolled a #{first_die}, a #{second_die}, a #{third_die}, a #{fourth_die}, and a #{fifth_die} for a total of #{sum}."
+
+  "<h1>2d10</h1>
   <p>#{outcome}</p>"
 end
