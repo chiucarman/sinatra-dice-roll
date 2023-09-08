acb50b8d7e3f018fa53c9adf083f082d3bc7c4ec
Carman Chiu <101212528+chiucarman@users.noreply.github.com>
2023-09-08 19:11:16 +0000

    created view template for one_twenty

--- dice.rb
+++ dice.rb
@@ -44,10 +44,9 @@ get("/dice/1/20") do
   first_die = rand(1..20)
   sum = first_die
 
-  outcome = "You rolled a #{first_die} for a total of #{sum}."
+  @outcome = "You rolled a #{first_die} for a total of #{sum}."
 
-  "<h1>1d20</h1>
-  <p>#{outcome}</p>"
+  erb(:one_twenty)
 end
 
 get("/dice/5/4") do
